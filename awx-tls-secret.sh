299a71a6bff5545361777a4f0778643a000f39a4
InverseBot <support@inverse.ca>
2025-07-14 19:03:48 +0000

    add modif

--- awx-tls-secret.sh
+++ awx-tls-secret.sh
@@ -2,4 +2,13 @@
 # install nginx-ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
 
+kubectl create ns awx
+
+kubectl config set-context --current --namespace=awx
+
 kubectl create secret tls awx-tls-secret   --cert=<(echo "$TENANTS_LB_CRT" | base64 -d)   --key=<(echo "$TENANTS_LB_KEY" | base64 -d)   -n awx
+
+cd awx-operator/
+
+make deploy
+kubectl apply -f ../awx-cr.yaml
--- awx_clone.sh
+++ awx_clone.sh
@@ -1,2 +1,5 @@
-git clone https://github.com/ansible/awx-operator.git .
+
+git clone https://github.com/ansible/awx-operator.git
+cd awx-operator
 git checkout 2.19.1
+cd --
